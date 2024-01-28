6af3d4158931b1a7a4ccd2d7f3bd9f678d588fea
wellheythereMJ <abetterplace@outlook.com>
2024-01-28 10:45:47 -0700

    updated logic

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -17,6 +17,9 @@ var start_btn = document.getElementById("start-btn");
 var quiz_area = document.getElementById("quiz-area");
 var question_h2 = document.getElementById("question");
 var choices_div = document.getElementById("choices");
+var startTime = 120;
+var timer = document.getElementById("timer");
+timer.textContent = startTime
 var quiz_array = [
   {
     question: "question1",
@@ -54,9 +57,19 @@ start_btn.addEventListener('click', function() {
 displayQuestion()
 
 })
-
-function timer () {
+// document.addEventListener('click', function(event){
+//   if((event.target.tagName =='BUTTON') && (event.target.textContent != "START")){
+//     event.preventDefault();
+//     answerChecker();
+//     console.log("bingus")
+//   }
+// })
 // count downs seconds from the clock stops when the seconds are empty
+function timer () {
+
+}
+
+function answerChecker () {
 
 }
 
@@ -64,11 +77,14 @@ function displayQuestion () {
 // add the question in the h2 and add the button choices to the div
 question_h2.textContent=quiz_array[array_index].question
 choices_div.innerHTML=""
+console.log(array_index)
 for (var i = 0; i < quiz_array [array_index].options.length; i++){
   var btn = document.createElement("button");
   btn.textContent = quiz_array[array_index].options[i];
+  console.log(array_index)
   // add on click to button
   
   choices_div.append(btn)
 }
+// array_index++
 }
